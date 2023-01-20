50b315ddcc22ea60e9b9b4fc69c3e87c2fc5f6c3
Ricky Esparza <RickyEsparza@Rickys-MacBook.local>
2023-01-19 23:07:13 -0700

    added more comments to server.js

--- server.js
+++ server.js
@@ -3,15 +3,22 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 
-/* Creating App with Express and setting up middleware */
+/* Creating App and setting up middleware */
+
+// App is created with Express.js
 const app = express();
+
+// PORT for server is set to 3001
 const PORT = process.env.PORT || 3001;
 
+// Middleware being applied to app
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
 /* GET routes */
+
+// Allows the user to get notes
 app.get('/api/notes/:id', (req, res) => {
     res.json(allNotes[req.params.id]);
 });
@@ -27,16 +34,20 @@ app.get('/api/notes', (req, res) => {
     });
 });
 
+// Sends the user to the notes page
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 })
 
+// Sends the user to the homepage
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 
 /* POST route */
+
+// This allows the user to create a new note for themselves
 app.post("/api/notes", (req, res) => {
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
         if (err) {
